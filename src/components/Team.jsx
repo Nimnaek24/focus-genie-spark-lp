b9762741917b3fc4d4f63ee4a34017c13423ee4d
Ashinshanee <159622226+Ashinshanee02@users.noreply.github.com>
2025-03-22 13:27:24 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -19,7 +19,7 @@ const teamMembers = [
   {
     name: "Sithu Prabhashvi",
     role: "Fullstack Developer",
-    image: "images/Sithu.jpg",
+    image: "images/sithu.jpg",
     github: "https://github.com/Sithuprabashvi",
     linkedin: "https://www.linkedin.com/in/sithu-yasaswara-797717331/",
     email: "https://github.com/Sithuprabashvi",
@@ -36,7 +36,7 @@ const teamMembers = [
   {
     name: "Chithya Dewmini",
     role: "Frontend Developer",
-    image: "images/Dewmini.jpg",
+    image: "images/dewmini.jpg",
     github: "https://github.com/dew0527",
     linkedin: "https://www.linkedin.com/in/kulasinge-dewmini-395a91292",
     email: "mailto:chithya.20231275@iit.ac.lk",
